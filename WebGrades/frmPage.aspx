1b0761eafd6b57c96d40fac5747cebe0d6ea554b
bradygrow <bradygrow@gmail.com>
2022-01-13 19:10:37 -0500

    Webgrader project. Pulls information from an oracle DB and populates different forms displaying grade information.

---
+++ WebGrades/frmPage.aspx
@@ -0,0 +1,68 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="frmPage.aspx.cs" Inherits="WebGrades.frmPage" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        #frmPage {
+            height: 662px;
+            z-index: 1;
+            left: 10px;
+            top: 15px;
+            position: absolute;
+            width: 1217px;
+        }
+    </style>
+</head>
+<body style="height: 741px">
+    <form id="frmPage" runat="server">
+            <asp:Label ID="Label1" runat="server" style="position: relative" Text="Username"></asp:Label>
+        <div style="z-index: 1; left: 10px; top: 15px; position: absolute; height: 19px; width: 1192px">
+        </div>
+        <asp:TextBox ID="txtUsername" runat="server" style="position: relative; top: 36px; left: -61px; height: 21px;"></asp:TextBox>
+        
+        <asp:Label ID="Label2" runat="server" style="position: relative" Text="Password"></asp:Label>
+        
+        <asp:TextBox ID="txtPassword" runat="server" style="position: relative; top: 39px; left: -58px;"></asp:TextBox>
+        <asp:Button ID="butLogin" runat="server" AutoPostBack="True" style="position: relative; top: 36px; left: -11px; height: 25px;" Text="Login" OnClick="butLogin_Click" />
+        <br />
+        <br />
+        <p>
+            &nbsp;</p>
+        <p>
+        <asp:Label ID="lbllogin" runat="server" style="position: relative"></asp:Label>
+        </p>
+        <asp:TextBox ID="txtSearch" style="position: relative; top: 42px; left: 124px; height: 17px;" runat="server"></asp:TextBox>
+        <hr />
+        <asp:Label ID="lblSearchInfo" runat="server" Text="Search by SID"></asp:Label>
+         <asp:Button ID="butSearch" runat="server" style="position: relative; top: 2px; left: 213px; height: 20px;" Text="Search" OnClick="butSearch_Click" />
+
+
+
+            
+       
+
+
+    <hr />
+            <asp:DropDownList ID="ddlclasses" style="position: relative; top: 90px; left: 12px;" runat="server" OnSelectedIndexChanged="ddlclasses_SelectedIndexChanged">
+            </asp:DropDownList>
+            <asp:Label ID="lblsearchedStudent" runat="server" Text="Label"></asp:Label>
+            <asp:GridView ID="gridGradeSummary" AutoGenerateColumns="true" style="position: relative; top: 117px; left: 14px;" runat="server">
+            </asp:GridView>
+
+
+
+            <asp:Button ID="butSubmit" style="position: relative; top: 6px; left: 474px; height: 75px; width: 66px;" runat="server" Text="Submit" OnClick="butSubmit_Click" />
+
+
+
+            <asp:GridView ID="gridgradedetail" style="position: relative; top: 162px; left: 87px;" runat="server">
+            </asp:GridView>
+
+
+
+    </form>
+    </body>
+</html>
---
+++ WebGrades/frmPage.aspx.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebGrades
+{
+
+    public partial class frmPage : System.Web.UI.Page
+    {
+        public NewGraderInfoService.NewGraderInfoServiceSoapClient NewGraderClient;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            NewGraderClient = new NewGraderInfoService.NewGraderInfoServiceSoapClient("NewGraderInfoServiceSoap");
+
+            if (Page.IsPostBack)
+            {
+                lbllogin.Text = "ispostback";
+                String token = (String)this.ViewState["Token"];
+
+            }
+            else
+            {
+                this.ViewState["Token"] = "";
+                lbllogin.Text = "is first load";
+            }
+        }
+
+        protected void butLogin_Click(object sender, EventArgs e)
+        {
+            String attempt = NewGraderClient.logIn(txtUsername.Text, txtPassword.Text, "Teacher");
+            if (attempt.StartsWith("ERROR"))
+            {
+                lbllogin.Text = "Login Failed!";
+            }
+            else
+            {
+
+
+                lbllogin.Text = "Successful Login!";
+                this.ViewState["Token"] = attempt;
+            }
+
+        }
+
+        protected void butSearch_Click(object sender, EventArgs e)
+        {
+            String SID = txtSearch.Text;
+            this.ViewState["SID"] = SID;
+            NewGraderInfoService.Student currentstudent = NewGraderClient.getStudent(SID, (String)this.ViewState["Token"]);
+            lblsearchedStudent.Text = currentstudent.lastname + ", " + currentstudent.firstname + " " + currentstudent.middleinitial + ".";
+            ddlclasses.DataSource = currentstudent.studentEnrollments;
+            ddlclasses.DataTextField = "COURSENUM";
+            ddlclasses.DataValueField = "OFFERINGID";
+            ddlclasses.DataBind();
+
+        }
+
+        protected void ddlclasses_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            String OFFERINGID = ddlclasses.SelectedValue;
+            int offeringid = Convert.ToInt32(OFFERINGID);
+            this.ViewState["OfferingID"] = offeringid;
+
+
+        }
+
+        protected void butSubmit_Click(object sender, EventArgs e)
+        {
+            NewGraderInfoService.StudentClassPerformance scp = NewGraderClient.getStudentClassPerformance((String)this.ViewState["SID"], (int)this.ViewState["OfferingID"], (String)this.ViewState["Token"]);
+
+            gridGradeSummary.Visible = true;
+            gridGradeSummary.DataSource = scp.studentGradeSummary;
+            gridGradeSummary.DataBind();
+
+            gridgradedetail.Visible = true;
+            gridgradedetail.DataSource = scp.studentGrades;
+            gridgradedetail.DataBind();
+        }
+    }
+}
+
